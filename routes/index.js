c661fb8996f31f80a1fffaf4b66d2a1d8492807b
purplesunset871019 <32476279+purplesunset871019@users.noreply.github.com>
2021-03-25 18:08:01 +0800

    3/25
    新建檔案、index logo、index導航列

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '駿金有限公司' });
 });
 
 module.exports = router;
