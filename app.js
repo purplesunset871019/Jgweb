92b3820925fccf8a230c6c16a586615f7428b087
purplesunset871019 <32476279+purplesunset871019@users.noreply.github.com>
2021-04-20 17:36:37 +0800

    4/20
    pro2-1剩content

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/product');
+var pro2_1Router = require('./routes/pro2-1');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/product', productRouter);
+app.use('/pro2-1', pro2_1Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
