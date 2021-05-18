e767ea7fbbcdcddd996741e6a3bc331f1b4c565e
purplesunset871019 <32476279+purplesunset871019@users.noreply.github.com>
2021-05-18 17:25:24 +0800

    5/18
    contact建立

---
+++ routes/contact.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: '駿金有限公司' });
+});
+
+module.exports = router;
